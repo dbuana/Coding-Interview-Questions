8edd38593595bafedbad53f93ce18268a2ca28c3
dbuana <46995714+dbuana@users.noreply.github.com>
2020-07-21 22:46:30 +0900

    Add files via upload

---
+++ Missing Number Question/missingNumbers.cs
@@ -0,0 +1,61 @@
+// 1). Find the missing number in the array
+namespace MissingNumberProblem {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            // The main program array - {3,7,1,2,8,4,5}
+            // With a function in dedication to organizing and a loop to constantly validate the information
+            
+            // The crucially important variable
+            int[] array = new int[] {3,7,1,2,8,4,5};
+            int[] expectedOutcome = new int[] {1,2,3,4,5,7,8};
+            int orderNumber = new Random int[array]; // Won't execute this value, still trying to improve this line
+            
+            // Following the variables; a loop is written
+            void organizing() {
+                for (array in expextedOutcome) {
+                    if (orderNumber == expectedOutcome) {
+                        return Console.WriteLine("Yes");
+                    } else {
+                        return None
+                    }
+                }
+           // The function with dedication to finding the missing number
+                void MissingNumber() {
+                     // The crucially important variable
+                    int[] array = new int[] {3,7,1,2,8,4,5};
+                    int[] expectedOutcome = new int[] {1,2,3,4,5,7,8};
+                    int orderNumber = new Random int[array]; // Won't execute this value, still trying to improve this line.
+                    // The long, but important statement; with dedication to validation/
+                    for (orderNumber in expectedOutcome) {
+                        if (orderNumber[0] == 1) {
+                            var accept;
+                            Console.WriteLine(accept);
+                        } if (orderNumber[1] == 2) {
+                            var accpet;
+                            Console.WriteLine(accept);
+                        } if (orderNumber[2] == 3) {
+                            var accept;
+                            Console.WriteLine(accept);
+                        } if (orderNumber[3] == 4) {
+                            var accept;
+                            Console.WriteLine(accept);
+                        } if (orderNumber[4] == 4) {
+                            var accept;
+                            Console.WriteLine(accept);
+                        } if (orderNumber[4] == 5) {
+                            var accept;
+                            Console.WriteLine(accept):
+                        } if (orderNumber[5] != 6) {
+                            finalResult = orderNumber.append(6);
+                            return Console.WriteLine(finalResult);
+                        }
+                   }
+               }
+           }
+       }
+   }
+}
+
